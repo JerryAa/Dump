ea737f003328f6f9f06205e52d5daef264d5670b
Jerry <jerrya15@yahoo.om>
2018-08-31 12:12:18 -0500

    Added delegate

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace Dump
 {
     class Program
     {
+		public delegate double myDel(int x); 
         static void Main(string[] args)
         {
+			try { 
+				int number = Convert.ToInt32(Console.ReadLine()); 
+			} 
+			catch (Exception e) 
+			{ 
+				Console.WriteLine(e.Message); 
+			} 
+			
             Console.WriteLine("Hello World!");
+	
+			myDel addd = (x) => x + 10; 
+			Console.WriteLine(addd.Invoke(20)); 
+
+			myDel counter = delegate(int x) { return Math.Pow(x, 10);}; 
         }
     }
 }
